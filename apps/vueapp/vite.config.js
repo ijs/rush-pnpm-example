c29c6ad3586d66b4781afbaa018975022f3b6e9b
jiangtao <w3cfuns@qq.com>
2021-11-26 15:00:02 +0800

    feat: docs update

--- apps/vueapp/vite.config.js
+++ apps/vueapp/vite.config.js
@@ -3,12 +3,12 @@ import vue from '@vitejs/plugin-vue'
 import Path from 'path'
 const isProd = process.env.NODE_ENV === 'production'
 const BASE = process.env.BASE || 'docs'
+console.log(BASE)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build:{
     outDir: Path.resolve(process.cwd(), '../../','docs'),//Specify the output directory (relative to project root).
   },
-  publicDir: Path.resolve(process.cwd(), '../../','docs/public'),
   base: isProd ? '/' : `/${BASE}`
 })
